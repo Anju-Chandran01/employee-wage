6639642dc86c3967e6fbf28fb8a0c96ed32c2813
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-02 20:05:02 +0530

    Added : Part time employee and wage using if-else

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,27 +5,26 @@ import java.util.Random;
 public class EmployeeWage {
 
     static final int WAGE_PER_HOUR = 20;
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
 
     public static void main(String[] args) {
         System.out.println("--- Welcome to Employee Wage Computation Program ---");
-        int attendance = (int)Math.floor(Math.random()*10)%2;
-        if(attendance == 1){
-            int empHrs = 8;
-            int empWage = empHrs * WAGE_PER_HOUR;
-            System.out.println("Employee is present");
-            System.out.println("Daily wage of employee is "+ empWage);
+        int empHrs = 0;
+        int empWage = 0;
+        int checkAttendance = (int)Math.floor(Math.random()*10)%3;
+        if(checkAttendance == IS_FULL_TIME){
+            System.out.println("Employee works for Full Time");
+            empHrs = 8;
+        }
+        else if(checkAttendance == IS_PART_TIME){
+            System.out.println("Employee works for Part Time");
+            empHrs = 4;
         }
         else{
             System.out.println("Employee is absent");
         }
+        empWage = empHrs * WAGE_PER_HOUR;
+        System.out.println("Employee Wage is "+ empWage);
     }
 }
-
-
-
-
-
-
-
-
-
