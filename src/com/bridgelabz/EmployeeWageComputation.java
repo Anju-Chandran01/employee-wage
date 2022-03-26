0268384df0b287eb32043f7640a2cd9607b5a144
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-26 00:13:44 -0700

    Added : Calculate Employee Wage for multiple Companies usingb Array List

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,32 +1,43 @@
 package com.bridgelabz;
 
-public class EmployeeWageComputation {
-    //constants
+import java.util.ArrayList;
+
+interface EmployeeWage {
+
+    void addCompanyEmpWage(String company ,int wagePerHour,int workingDays,int workingHours);
+    void computeEmpWage();
+}
+
+public class EmployeeWageComputation implements EmployeeWage {
+
+    //CONSTANTS
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
-    private int numOfCompany = 0;
-    private final CompanyEmpWage[] companyEmpWageArray;
+    private final ArrayList<CompanyEmpWage> companies;
 
     public EmployeeWageComputation() {
-        companyEmpWageArray = new CompanyEmpWage[5];
+        companies = new ArrayList<>();
     }
 
-    private void addCompanyEmpWage(String company ,int wagePerHour,int workingDays,int workingHours){
-        companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company,wagePerHour,workingDays,workingHours);
-        numOfCompany++;
+    public void addCompanyEmpWage(String company ,int wagePerHour,int workingDays,int workingHours){
+        companies.add(new CompanyEmpWage(company,wagePerHour,workingDays,workingHours));
     }
 
-    private void computeEmpWage() {
-        for (int i =0; i < numOfCompany; i++) {
-            companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-            System.out.println(companyEmpWageArray[i]);
+    public void computeEmpWage() {
+        for (CompanyEmpWage company : companies) {
+            int totalWage = computeEmpWage(company);
+            company.setTotalEmpWage(totalWage);
+            System.out.println(company);
         }
     }
-    //Calculating monthly wage of employee
+
+    //CALCULATING MONTHLY WAGE OF EMPLOYEE
     private int computeEmpWage(CompanyEmpWage companyEmpWage){
-        //variables
+
+        //VARIABLES
         int empHours, totalWorkingDays=0, totalEmpHours=0;
+
         while (totalEmpHours <= companyEmpWage.workingHours && totalWorkingDays < companyEmpWage.workingDays){
             totalWorkingDays++;
             int empCheck=(int) Math.floor(Math.random() * 10) % 3;
@@ -37,12 +48,14 @@ public class EmployeeWageComputation {
             };
             totalEmpHours += empHours;
         }
-        System.out.println("total employee working hour - "+totalEmpHours);
-        System.out.println("total employee working days - "+totalWorkingDays);
+
+        System.out.println("Total employee working hour - "+totalEmpHours);
+        System.out.println("Total employee working days - "+totalWorkingDays);
         return totalEmpHours * companyEmpWage.wagePerHour;
     }
+
     public static void main(String[] args){
-        System.out.println("Welcome to Employee Wage Computation");
+        System.out.println("--- Welcome to Employee Wage Computation ---");
         EmployeeWageComputation empWage = new EmployeeWageComputation();
         empWage.addCompanyEmpWage("Dmart",20,20,100);
         empWage.addCompanyEmpWage("Reliance",10,25,150);
